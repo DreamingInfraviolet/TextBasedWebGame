dcb5b8ed17bfe408924536c6e39891bf50752785
Anima <lightofanima@gmail.com>
2015-11-17 19:32:39 +0000

    Made further progress

--- site/scripts/game.js
+++ site/scripts/game.js
@@ -1,19 +1,13 @@
-been_to_corridor=true;
-
-$(document).ready(function() {
-
+$(document).ready(function()
+{
     writeLine("Wake up, Neo.", false);
 
-    $("form").submit(function() {
-        var input = $("#console_input").val();
-        if(input=="help")
-        {
-            writeLine("Help yourself, smoochie", false);
-        }
-        else
-        {
-           writeLine(input, true);
-        }
+    $("form").submit(function()
+    {
+        var input = cleanInput($("#console_input").val());
+        writeLine(input, true);
+        
+        setTimeout(function() {handleInput(input)}, 500);
 
         //Scroll down. Setting this to a big value, as it seems to designate the limit of scrolling.
         $("#console_text").scrollTop(2000000000);
@@ -30,6 +24,19 @@ $(document).ready(function() {
     }, 5000);
 });
 
+function cleanInput(input)
+{
+    return input.trim();
+}
+
+function handleInput(input)
+{
+    if(input=="help")
+        writeLine("Help yourself, smoochie", false);
+    else if(input=="cls" || input=="clear")
+        clearConsole();
+}
+
 function writeLine(text, isByUser)
 {
     var c = isByUser ? "user_output" : "console_output"
@@ -38,5 +45,5 @@ function writeLine(text, isByUser)
         .removeAttr("id")
         .attr("class", c)
         .insertBefore("#placeholder")
-        .fadeIn(1000);
+        .fadeIn(1400);
 }
