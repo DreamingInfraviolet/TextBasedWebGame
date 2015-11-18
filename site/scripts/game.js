b6d02fe9e4df7ce530fcb317180c7b14b8376d1c
Anima <lightofanima@gmail.com>
2015-11-18 09:17:24 +0000

    Added waiting notifier
    When input is disabled, the user will get an icon telling them so, which
    will flash if they try to enter input anyway ^.^

--- site/scripts/game.js
+++ site/scripts/game.js
@@ -1,17 +1,45 @@
+var acceptInput = false;
+
+var waitingNotifierBlinkTime = 200;
+var lastBlinkStartTime = Date.now()-waitingNotifierBlinkTime*4;
+
+function setAcceptInput(value)
+{
+        acceptInput = value;
+        if(value==true)
+            $("#waiting_notifier").fadeOut(3000);
+        else
+            $("#waiting_notifier".fadeIn(3000));
+}
+
 $(document).ready(function()
 {
     writeLine("Wake up, Neo.", false);
 
     $("form").submit(function()
     {
-        var input = cleanInput($("#console_input").val());
-        writeLine(input, true);
-        
-        setTimeout(function() {handleInput(input)}, 500);
+        if(acceptInput==true)
+        {
+            var input = cleanInput($("#console_input").val());
+            writeLine(input, true);
+            
+            setTimeout(function() {handleInput(input)}, 500);
 
-        //Scroll down. Setting this to a big value, as it seems to designate the limit of scrolling.
-        $("#console_text").scrollTop(2000000000);
-        $("#console_input").val("");
+            //Scroll down. Setting this to a big value, as it seems to designate the limit of scrolling.
+            $("#console_text").scrollTop(2000000000);
+            $("#console_input").val("");
+        }
+        else
+        {
+            if(Date.now()>lastBlinkStartTime+waitingNotifierBlinkTime*4)
+            {
+                    $("#waiting_notifier").fadeOut(waitingNotifierBlinkTime)
+                                          .fadeIn(waitingNotifierBlinkTime)
+                                          .fadeOut(waitingNotifierBlinkTime)
+                                          .fadeIn(waitingNotifierBlinkTime);
+                lastBlinkStartTime = Date.now();
+            }
+        }
     });
 
     setTimeout(function()
@@ -20,6 +48,7 @@ $(document).ready(function()
         setTimeout(function()
         {
             writeLine("Follow the white rabbit.");
+            setAcceptInput(true);
         }, 5000);
     }, 5000);
 });
