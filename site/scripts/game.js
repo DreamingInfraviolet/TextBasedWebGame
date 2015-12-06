4dd940ef8717b527cb8cb729378b21857d6c825c
Anima <lightofanima@gmail.com>
2015-12-06 14:57:49 +0000

    Added handling of impossible move choices

--- site/scripts/game.js
+++ site/scripts/game.js
@@ -5,10 +5,13 @@
 
 
 //A node class to represent a graph.
-function Node(txt)
+function Node(name, txt, secondVisitTxt)
 {
+    this.name = name;
     this.text = txt;
+    this.secondVisitTxt = secondVisitTxt;
     this.branches={}; //Every object is a hash table......... .__.
+    this.visited = false;
 }
 
 //Add a node that can be reached if followed by the given transition.
@@ -21,7 +24,10 @@ Node.prototype.addNode = function(node, transition)
 //Undefined if no such node exists.
 Node.prototype.follow = function(transition)
 {
-    return this.branches[transition];
+    if(transition in this.branches)
+        return this.branches[transition];
+    else
+        return null;
 };
 
 //Change the text of a particular node
@@ -33,20 +39,32 @@ Node.prototype.setText = function(txt)
 
 Node.prototype.print = function()
 {
-    Console.writeSlowly(this.text+"\n", false);
+    Console.writeSlowly(this.name, false);
+    
+    if(!this.visited)
+    {
+        Console.writeSlowly(this.text, false);
+    }
+    else
+    {
+        if(this.secondVisitTxt!==undefined)
+        {
+            Console.writeSlowly(this.secondVisitTxt);
+        }
+    }
 };
 
 
 
 function initGraph()
 {
-    var root = new Node("You find yourself looking at a dark console window, unsure of what to do. Naturally you grow bored and wish to leave. East of you is the kitchen, while north is the exit from the house. Where will you go?");
+    var root = new Node("Computer Room", "You find yourself looking at a dark console window, unsure of what to do. Naturally you grow bored and wish to leave. East of you is the kitchen, while north is the exit from the house. Where will you go?", "Back here, huh.");
     
-    var kitchen = root.addNode(new Node("Having entered the kitchen, you realise that you're kinda bored here as well. All that you can really do is go back. What now, pumpkin?"), "e");
+    var kitchen = root.addNode(new Node("Kitchen", "Having entered the kitchen, you realise that you're kinda bored here as well. All that you can really do is go back. What now, pumpkin?"), "e");
     
     kitchen.addNode(root, "w");
     
-    var outside = root.addNode(new Node("As you step outside, the frigid air assaults your lungs, making you want to go back inside. But lo! You locked yourself out! How typical! Now you're stuck there. GOOD JOB -.-"), "n");
+    var outside = root.addNode(new Node("Outside", "As you step outside, the frigid air assaults your lungs, making you want to go back inside. But lo! You locked yourself out! How typical! Now you're stuck there. GOOD JOB -.-"), "n");
     
     return root;
 }
@@ -60,6 +78,7 @@ $(function ()
     Console.setAcceptInput(true);
     
     graph.print();
+    graph.visited=true;
     
     
     $("form").submit(function()
@@ -125,8 +144,15 @@ function handleInput(input)
 
 function takeTransition(prefix, transition)
 {
-    Console.writeFade(prefix,false);
-    graph=graph.follow(transition);
-    graph.print();
+    if(graph.follow(transition)!==null)
+    {    
+        Console.writeFade(prefix,false);
+        graph=graph.follow(transition);
+        graph.print();
+        graph.visited = true;
+    }
+    else
+        Console.writeFade("Can't go there!");
+    
     return true;
 }
